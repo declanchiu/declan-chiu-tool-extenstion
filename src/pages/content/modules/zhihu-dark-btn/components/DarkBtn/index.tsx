fff2243718567829d674b63001924c2a4f981555
Declan_Chiu <1282117217@qq.com>
2023-01-29 11:34:54 +0800

    doc: 📝 补充 README 对知乎按钮的描述

--- src/pages/content/modules/zhihu-dark-btn/components/DarkBtn/index.tsx
+++ src/pages/content/modules/zhihu-dark-btn/components/DarkBtn/index.tsx
@@ -5,55 +5,55 @@ import DarkIcon from "../../../../../../assets/content/zhihu-dark-icon.png";
 import "./index.css";
 
 const ZhihuDarkBtn = () => {
-  onMount(() => {
-    test();
-  })
+  // onMount(() => {
+  //   test();
+  // })
 
 
-  const test = () => {
-    const TopstoryItemList = document.getElementsByClassName("TopstoryItem");
-    const list = Array.from(TopstoryItemList);
+  // const test = () => {
+  //   const TopstoryItemList = document.getElementsByClassName("TopstoryItem");
+  //   const list = Array.from(TopstoryItemList);
 
-    const tabBarList = list.map((item) => {
-      return item.getElementsByClassName("ContentItem-actions");
-    });
+  //   const tabBarList = list.map((item) => {
+  //     return item.getElementsByClassName("ContentItem-actions");
+  //   });
 
-    const metaList = list.map((item) => {
-      return item.getElementsByTagName("meta");
-    });
+  //   const metaList = list.map((item) => {
+  //     return item.getElementsByTagName("meta");
+  //   });
 
-    let toUrlList: any[] = [];
-    metaList.forEach((item) => {
-      const itemArr = Array.from(item);
-      itemArr.forEach((son) => {
-        const isUrl = son.getAttribute("itemprop") === "url";
-        if (isUrl) {
-          const articleUrl = son.getAttribute("content") + '?theme=dark';
-          toUrlList.push(articleUrl);
-        }
-      });
-    });
+  //   let toUrlList: any[] = [];
+  //   metaList.forEach((item) => {
+  //     const itemArr = Array.from(item);
+  //     itemArr.forEach((son) => {
+  //       const isUrl = son.getAttribute("itemprop") === "url";
+  //       if (isUrl) {
+  //         const articleUrl = son.getAttribute("content") + '?theme=dark';
+  //         toUrlList.push(articleUrl);
+  //       }
+  //     });
+  //   });
 
-    const toArticle = (index: number) => {
-      window.open(toUrlList[index]);
-    }
+  //   const toArticle = (index: number) => {
+  //     window.open(toUrlList[index]);
+  //   }
 
-    tabBarList.forEach((item, index) => {
-      if (item[0]) {
-        item[0].classList.add("_self-injection-zhihu_btn")
-        item[0].append(
-          // @ts-ignore
-          <img
-            src={chrome.runtime.getURL(DarkIcon)}
-            class="dark-btn_wrapper"
-            onClick={() => toArticle(index)}
-          />
-        );
-      }
-    });
+  //   tabBarList.forEach((item, index) => {
+  //     if (item[0]) {
+  //       item[0].classList.add("_self-injection-zhihu_btn")
+  //       item[0].append(
+  //         // @ts-ignore
+  //         <img
+  //           src={chrome.runtime.getURL(DarkIcon)}
+  //           class="dark-btn_wrapper"
+  //           onClick={() => toArticle(index)}
+  //         />
+  //       );
+  //     }
+  //   });
 
-    console.log("查看跳转链接", tabBarList);
-  };
+  //   console.log("查看跳转链接", tabBarList);
+  // };
 
   return (
     <></>
